a678bc0c08a8e0a3182377b8d57dc94c3b860be3
Vasilena Todorova <vasilenaph@users.noreply.github.com>
2024-01-20 11:21:52 +0200

    Show dynamic movies on home page

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -9,7 +9,7 @@ router.get('/create', (req, res) => {
 router.post('/create', (req, res) => {
     const newMovie = req.body;
     movieService.create(newMovie);
-    res.send('Movie should be created');
+    res.redirect('/');
 });
 
 module.exports = router;
--- src/services/movieService.js
+++ src/services/movieService.js
@@ -8,6 +8,11 @@ const movies = [{
     description: "The youngest of King Triton's daughters, Ariel is a beautiful and spirited young mermaid with a thirst for adventure. Longing to find out more about the world beyond the sea, Ariel visits the surface and falls for the dashing Prince Eric. Following her heart, she makes a deal with the evil sea witch, Ursula, to experience life on land."
 }];
 
+exports.getAll = () => {
+    return movies.slice();
+    // return [...movies];
+};
+
 exports.create = (movieData) => {
     movies.push(movieData);
 };
