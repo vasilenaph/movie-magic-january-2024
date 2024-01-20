32f60d3783f76cb7ae9909a5509f0edb12020882
Vasilena Todorova <vasilenaph@users.noreply.github.com>
2024-01-20 10:59:24 +0200

    Add hardcoded movie

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,4 +1,12 @@
-const movies = [];
+const movies = [{
+    title: 'Jungle Cruise ',
+    genre: 'Adventure',
+    director: 'Lucas',
+    date: '2019',
+    imageUrl: '/img/jungle-cruise.jpeg',
+    rating: '5',
+    description: "The youngest of King Triton's daughters, Ariel is a beautiful and spirited young mermaid with a thirst for adventure. Longing to find out more about the world beyond the sea, Ariel visits the surface and falls for the dashing Prince Eric. Following her heart, she makes a deal with the evil sea witch, Ursula, to experience life on land."
+}];
 
 exports.create = (movieData) => {
     movies.push(movieData);
