a28c9d7992f17a8aa9066e75690afff54de1b36f
Vasilena Todorova <vasilenaph@users.noreply.github.com>
2024-01-20 20:13:39 +0200

    Add rating to details page

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -15,9 +15,12 @@ router.post('/create', (req, res) => {
 router.get('/movies/:movieId', (req, res) => {
     const movieId = req.params.movieId;
     const movie = movieService.getOne(movieId);
+    // movie.rating = new Array(Number(movie.rating)).fill(true);
 
-    console.log(movie);
-    res.render('details', {movie});
+    // TODO: This is not perfect, use handlebars helpers
+    movie.ratingStars = '&#x2605;'.repeat(movie.rating);
+
+    res.render('details', { movie });
 });
 
 module.exports = router;
