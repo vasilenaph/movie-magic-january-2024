8d5c3082bb8d8f6d0976a5d1e247fc04cf83c48c
Vasilena Todorova <vasilenaph@users.noreply.github.com>
2024-01-20 20:24:03 +0200

    Show dynamic movies on search page

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -11,6 +11,11 @@ router.get('/about', (req, res) => {
     res.render('about');
 });
 
+router.get('/search', (req, res) => {
+    const movies = movieService.getAll();
+    res.render('search', { movies });
+});
+
 router.get('/404', (req, res) => {
     res.render('404');
 });
