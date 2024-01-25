4f56b04c1f6e96b8dc35c53b977ce52b4f4e9a56
Vasilena Todorova <vasilenaph@users.noreply.github.com>
2024-01-25 12:02:50 +0200

    Use mongoose for movie cerate

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -6,10 +6,17 @@ router.get('/create', (req, res) => {
     res.render('create');
 });
 
-router.post('/create', (req, res) => {
+router.post('/create', async (req, res) => {
     const newMovie = req.body;
-    movieService.create(newMovie);
-    res.redirect('/');
+
+    try {
+        await movieService.create(newMovie);
+
+        res.redirect('/');
+    } catch (err) {
+        console.log(err.message);
+        res.redirect('/create');
+    }
 });
 
 router.get('/movies/:movieId', (req, res) => {
