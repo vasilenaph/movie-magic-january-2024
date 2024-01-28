a0c8ec3677a629f63c54aa1f159f7ea2955ac8e2
Vasilena Todorova <vasilenaph@users.noreply.github.com>
2024-01-28 14:30:01 +0200

    Show movie data on attach page

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -30,8 +30,10 @@ router.get('/movies/:movieId', async (req, res) => {
     res.render('details', { movie });
 });
 
-router.get('/movies/:movieId/attach', (req, res) => {
-    res.render('movie/attach');
+router.get('/movies/:movieId/attach', async (req, res) => {
+    const movie = await movieService.getOne(req.params.movieId).lean();
+
+    res.render('movie/attach', { ...movie });
 });
 
 module.exports = router;
